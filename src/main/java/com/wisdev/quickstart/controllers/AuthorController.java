30af3039a34a2d12a9c765ab3cdbebf5d32442f0
Akhdani Dev <akhdani.dev@gmail.com>
2024-09-02 22:10:48 +0700

    update:change to v5

--- src/main/java/com/wisdev/quickstart/controllers/AuthorController.java
+++ src/main/java/com/wisdev/quickstart/controllers/AuthorController.java
@@ -56,7 +56,7 @@ public class AuthorController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public ResponseEntity<String> index(){
-        String msg = "Hello from v4";
+        String msg = "Hello from v5";
         return ResponseEntity.ok(msg);
     }
 }
